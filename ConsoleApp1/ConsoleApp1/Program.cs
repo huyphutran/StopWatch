b95515652f651d65780c44a1f80754d74eb2ea7f
Phu Tran <tranhuyphuhp75@gmail.com>
2023-10-02 23:52:51 +0700

    Add A StopWatch

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new StopWatch();
+            Console.WriteLine("Press S to Start the stopwatch");
+            Console.WriteLine("Press P to Stop the stopwatch");
+            Console.WriteLine("Press Q to Start the stopwatch");
+
+            while (true)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                char key = char.ToUpper(consoleKeyInfo.KeyChar);
+                   switch (key)
+                {
+                    case 'S':
+                            stopWatch.Start();
+                        break;
+
+                    case 'P':
+                        stopWatch.Stop();
+                        break;
+                    case 'Q':
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid please press the following key in Structure");
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/StopWatch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class StopWatch
+    {
+        private DateTime StartTime;
+        private bool IsRunning;
+
+        public void Start() {
+
+            if(!IsRunning){
+                StartTime = DateTime.Now;
+                IsRunning = true;
+                Console.WriteLine($"Run time: {StartTime:HH:MM:SS.fff}");
+                GetIntervalTime();
+            }
+            else
+            {
+                Console.WriteLine("Stopwatch is already running.");
+            }
+
+        }
+        public void Stop()
+        {
+
+            if (IsRunning)
+            {
+                IsRunning = false;
+                TimeSpan _timeSpan = DateTime.Now - StartTime;
+                Console.WriteLine($"Elapsed Time:{_timeSpan.TotalSeconds:F2} second");
+               
+            }
+
+        }
+
+        public void GetIntervalTime()
+        {
+            while (IsRunning)
+            {
+                TimeSpan _timeSpan = DateTime.Now - StartTime;
+                Console.WriteLine($"Elapsed Time:{_timeSpan.TotalSeconds:F2} second");
+                Thread.Sleep(1000);
+            }
+
+        }
+
+
+    }
+}
